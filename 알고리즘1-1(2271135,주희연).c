0e9d3be5604545411051513d83b95777ca0700ab
Renee Joo <renee.joo03@gmail.com>
2023-08-31 15:26:40 +0900

    hello

--- 알고리즘1-1(2271135,주희연).c
+++ 알고리즘1-1(2271135,주희연).c
@@ -3,24 +3,39 @@
 /*
 공식
 
--노드 n, 간선 n-1 >> 루트 노드 제외 모두 부모 트리 가짐
--높이, 최대 n, 최소 log(n+1)
+- 노드 n, 간선 n-1 >> 루트 노드 제외 모두 부모 트리 가짐
+- 높이, 최대 n, 최소 log(n+1)
 
--인덱스, 부모 노드 i/2, 왼 자식 2i, 오른 자식 2i+1
+- 인덱스, 부모 노드 i/2, 왼 자식 2i, 오른 자식 2i+1
 
+이진 탐색트리
+- 왼<중<오
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
+
+#define num1 100
+#define num2 200
+
+typedef struct {
+	
+	char word[num1];
+	char mean[num2];
+};
 
 typedef struct TreeNode {
 	int data;
 	struct TreeNode *right, *left;
 }TreeNode;
 
+
 /*
-	n1
-	/
+
 
 */
-TreeNode n1{ 1, NULL, NULL };
+
+
+// 1차 정정 
+// 내용 추가
