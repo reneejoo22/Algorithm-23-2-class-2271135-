d7e476968558df68a890694aba85f088778320b7
Renee Joo <renee.joo03@gmail.com>
2023-09-01 23:25:23 +0900

    순회_전중후 완

--- 알고리즘1-1(2271135,주희연).c
+++ 알고리즘1-1(2271135,주희연).c
@@ -1,3 +1,77 @@
+// 알고리즘 1주차
+//자료구조 복습_트리 구현
+
+#include <stdio.h>
+//#include <stdlib.h>
+//#include <memory.h>
+
+typedef struct TreeNode {
+	char data;
+	struct TreeNode *left, *right;
+}TreeNode;
+
+/*
+		 D(6)
+	   /     \
+	  A(4)    G(5)
+	 /       /  \
+   B(1)   H(2)  I(3)
+
+*/
+
+TreeNode n1 = { 'B',NULL,NULL };
+TreeNode n2 = { 'H',NULL,NULL };
+TreeNode n3 = { 'I',NULL,NULL };
+TreeNode n4 = { 'A',&n1,NULL };
+TreeNode n5 = { 'G',&n2, &n3 };
+TreeNode n6 = { 'D',&n4,&n5 };
+
+TreeNode *root = &n6;
+
+void preorder(TreeNode* root) {	//전위순환_ 중왼오
+	// D AB GHI
+	if (root != NULL) {
+		printf("%c ", root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+void inorder(TreeNode* root) {	//중위순환_ 왼중오
+	// 
+	if (root != NULL) {
+		inorder(root->left);
+		printf("%c ", root->data);
+		inorder(root->right);
+	}
+}
+
+void postorder(TreeNode* root) {	//후위순환_ 왼오중
+	// BA HGI D
+	if (root != NULL) {
+		postorder(root->left);  //왼
+		postorder(root->right);  //오
+		printf("%c ", root->data);  //중
+	}
+}
+
+
+
+int main() {
+
+	printf("전위순환\n");
+	preorder(root);
+	printf("\n");
+
+	printf("중위순환\n");
+	inorder(root);
+	printf("\n");
+
+	printf("후위순환\n");
+	postorder(root);
+	printf("\n");
+}
+
 //트리 복습
 
 /*
@@ -11,31 +85,3 @@
 이진 탐색트리
 - 왼<중<오
 */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
-
-#define num1 100
-#define num2 200
-
-typedef struct {
-	
-	char word[num1];
-	char mean[num2];
-};
-
-typedef struct TreeNode {
-	int data;
-	struct TreeNode *right, *left;
-}TreeNode;
-
-
-/*
-
-
-*/
-
-
-// 1차 정정 
-// 내용 추가
