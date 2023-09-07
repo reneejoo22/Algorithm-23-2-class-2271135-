b85cc5755cf8b319c620db420aa01ea0ed2a815e
Renee Joo <renee.joo03@gmail.com>
2023-09-07 17:27:05 +0900

    반복 탐색&순회 탐색 구현

--- 알고리즘1-1(2271135,주희연).c
+++ 알고리즘1-1(2271135,주희연).c
@@ -1,9 +1,12 @@
+// 올기긱
+
 // 알고리즘 1주차
-//자료구조 복습_트리 구현
+//자료구조 복습_트리 구현_ 탐색
+
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <memory.h>
+
 
 typedef struct TreeNode {
 	char data;
@@ -11,77 +14,124 @@ typedef struct TreeNode {
 }TreeNode;
 
 /*
+* 영어 데이터로 구성된 트리
 		 D(6)
 	   /     \
 	  A(4)    G(5)
 	 /       /  \
    B(1)   H(2)  I(3)
+   */
 
+
+TreeNode m1 = { 'B',NULL,NULL };
+TreeNode m2 = { 'H',NULL,NULL };
+TreeNode m3 = { 'I',NULL,NULL };
+TreeNode m4 = { 'A',&m1,NULL };
+TreeNode m5 = { 'G',&m2, &m3 };
+TreeNode m6 = { 'D',&m4,&m5 };
+
+TreeNode *root = &m6;
+
+
+typedef struct TreeNode2 {
+	int data;
+	struct TreeNode2* left, * right;
+}TreeNode2;
+
+/*
+ 숫자 데이터로 구성된 트리
+		 D(6)
+	   /     \
+	  A(4)    G(5)
+	 /       /  \
+   B(1)   H(2)  I(3)
+   */
+
+TreeNode2 n1 = { 1,NULL,NULL };
+TreeNode2 n2 = { 2,NULL,NULL };
+TreeNode2 n3 = { 3,NULL,NULL };
+TreeNode2 n4 = { 4,&n1,NULL };
+TreeNode2 n5 = { 5,&n2, &n3 };
+TreeNode2 n6 = { 6 ,&n4,&n5 };
+
+TreeNode2* roott = &n6;
+
+//TreeNode (문자)탐색 함수
+/*
+ 두가지 경우
+ 1. 아이템이 루트보다 크거나 작은 경우
+ 2. 아이템이 루트와 값이 일치하는 경우
+ **루트가 빈 값이 아닌지 확인
+
+ 아래 방식은 반복적인 방식
 */
 
-TreeNode n1 = { 'B',NULL,NULL };
-TreeNode n2 = { 'H',NULL,NULL };
-TreeNode n3 = { 'I',NULL,NULL };
-TreeNode n4 = { 'A',&n1,NULL };
-TreeNode n5 = { 'G',&n2, &n3 };
-TreeNode n6 = { 'D',&n4,&n5 };
+TreeNode* search(TreeNode* root, char item) {
 
-TreeNode *root = &n6;
+	TreeNode* tmp = root;
 
-void preorder(TreeNode* root) {	//전위순환_ 중왼오
-	// D AB GHI
-	if (root != NULL) {
-		printf("%c ", root->data);
-		preorder(root->left);
-		preorder(root->right);
+	while (tmp != NULL) {
+		if (item == tmp->data)return tmp; //루트에서 바로 찾으면 값 반환
+		else if (item < tmp->data)
+			tmp = tmp->left;
+		else
+			tmp = tmp->right;
 	}
+	return NULL;
 }
 
-void inorder(TreeNode* root) {	//중위순환_ 왼중오
-	// 
-	if (root != NULL) {
-		inorder(root->left);
-		printf("%c ", root->data);
-		inorder(root->right);
+//TreeNode2 (숫자)탐색 함수
+/*
+ 두가지 경우
+ 1. 아이템이 루트보다 크거나 작은 경우
+ 2. 아이템이 루트와 값이 일치하는 경우
+ **루트가 빈 값이 아닌지 확인
+
+ 아래 방식은 순환적인 방식
+*/
+
+int explore2(int item, TreeNode2* root) {	//숫자용
+
+	if (root == NULL)return 0;	//루트 값에 아무것도 없으면 돌려보내기
+
+	if (root->data == item)return 1;	// 입력받은 root가 item이랑 바로 일치하면 True 돌려보내기
+
+	if (root->data > item) {
+		return explore2(item, root->left);
+	}
+	else {
+		return explore2(item, root->right);
 	}
 }
-
-void postorder(TreeNode* root) {	//후위순환_ 왼오중
-	// BA HGI D
-	if (root != NULL) {
-		postorder(root->left);  //왼
-		postorder(root->right);  //오
-		printf("%c ", root->data);  //중
-	}
-}
-
 
 
 int main() {
 
-	printf("전위순환\n");
-	preorder(root);
-	printf("\n");
+	// explore 이용
+	char find;
 
-	printf("중위순환\n");
-	inorder(root);
-	printf("\n");
+	printf("탐색할 문자 입력-> ");
+	scanf("%c", &find);
 
-	printf("후위순환\n");
-	postorder(root);
-	printf("\n");
+	if (NULL != search(&m6, find)) {
+		printf("%c 찾음\n", find);
+	}
+	else printf("%c 없음\n", find);
+
+	// explore 2 이용/////////////////////////
+
+	int finding;
+
+	printf("탐색할 수 입력-> ");
+	scanf("%d", &finding);
+	//printf("%d 입력받음", finding);
+	
+	int result2 = explore2(finding, &n6);
+
+	if (result2 == 1) {
+		printf("%d 찾음", finding);
+	}
+	else { printf("%d 없음", finding); }
+
 }
-
-//트리 복습
-
-/*
-공식
-
-- 노드 n, 간선 n-1 >> 루트 노드 제외 모두 부모 트리 가짐
-- 높이, 최대 n, 최소 log(n+1)
-
-- 인덱스, 부모 노드 i/2, 왼 자식 2i, 오른 자식 2i+1
-
-이진 탐색트리
-- 왼<중<오
-*/
+	
